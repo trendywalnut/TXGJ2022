8ab8159e97e42aa229147590b48ec424ebe50f46
trendywalnut <connorb23@utexas.edu>
2022-09-25 16:57:51 -0500

    LOSE SCENE

---
+++ Assets/Scripts/ResetPlayScene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetPlayScene : MonoBehaviour
+{
+    
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene("Play Scene");
+    }
+}
